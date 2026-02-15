d40259d561629578d2ff88564c3ec6229fae4d31
manishdahal450 <manish.dahal450@gmail.com>
2026-02-15 21:56:19 +0545

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const skillFills = document.querySelectorAll(".skill-fill");
+let skillsAnimated = false;
+
+window.addEventListener("scroll", () => {
+  const aboutSection = document.querySelector("#about");
+  const sectionPos = aboutSection.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 1.2;
+
+  if (sectionPos < screenPos && !skillsAnimated) {
+    skillFills.forEach((bar) => {
+      const width = bar.getAttribute("data-width");
+      bar.style.width = width;
+    });
+    skillsAnimated = true; // ensures animation happens only once
+  }
+});
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const icon = toggleBtn.querySelector("i");
+
+// Load saved theme on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  }
+});
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+    localStorage.setItem("theme", "dark");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+    localStorage.setItem("theme", "light");
+  }
+});
